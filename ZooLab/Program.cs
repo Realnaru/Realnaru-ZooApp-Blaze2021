24002af0a84c21052e8e8b1ec8891973219bc310
Ruslan O <realnaru@gmail.com>
2021-06-08 01:14:56 +0800

    remove empty comment

--- ZooLab/Program.cs
+++ ZooLab/Program.cs
@@ -7,7 +7,7 @@ namespace ZooLab
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Hello World!");//
+            Console.WriteLine("Hello World!");
         }
     }
 }
